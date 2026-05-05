c283fa5c78c98e711b5621df95ae227b9570773a
Sojib Ahmed <sojiba1500@gmail.com>
2026-05-05 21:59:32 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js"; // Firestore ব্যবহার করুন
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD_TOgrehY8a5JD55FXHdRiFcZhHxctDkM",
   authDomain: "seen-a1d92.firebaseapp.com",
-  databaseURL: "https://seen-a1d92-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "seen-a1d92",
-  storageBucket: "seen-a1d92.appspot.com",
+  storageBucket: "seen-a1d92.firebasestorage.app", // আপডেট করা হয়েছে
   messagingSenderId: "868584044532",
   appId: "1:868584044532:web:51f07436a883f88332eba1"
 };
@@ -18,5 +17,5 @@ const app = initializeApp(firebaseConfig);
 
 // Export services
 export const auth = getAuth(app);
-export const db = getDatabase(app);
+export const db = getFirestore(app); // Firestore এক্সপোর্ট করুন
 export const storage = getStorage(app);
